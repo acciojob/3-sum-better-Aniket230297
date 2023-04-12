369f40ab3b3685c18c6fc70ebb9e7a2388430d44
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-04-12 09:06:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort(function (a, b) {
+    return a - b;
+  });
+  let i, j, k;
+  let nearest_target = 0,
+    least_diff = Number.MAX_VALUE;
+  for (i = 0; i < arr.length - 2; i++) {
+    j = i + 1;
+    k = arr.length - 1;
+    while (j < k) {
+      let sum = Number(arr[i]) + Number(arr[j]) + Number(arr[k]);
+      let diff = Math.abs(sum - target);
+      if (diff < least_diff) {
+        least_diff = diff;
+        nearest_target = sum;
+      }
+      if (sum == target) return nearest_target;
+      else if (sum > target) k--;
+      else j++;
+    }
+  }
+  return nearest_target;
   
 }
 
